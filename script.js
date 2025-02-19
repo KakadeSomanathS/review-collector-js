1c8740f7f9e3574b74f98b55199fa55fe5c42dfa
Somanath Shahaji kakade <somanath.kakade5@gmail.com>
2025-02-19 12:31:24 +0530

    added the edit feature

--- script.js
+++ script.js
@@ -2,29 +2,55 @@ const inputFiled = document.querySelector("#inputFiled");
 const inputBtn = document.querySelector("#addBtn");
 const menuItems = document.querySelector("#menuItems");
 
-
-
+const task = [];
 
 inputBtn.addEventListener("click", () => {
-  const value = inputFiled.value;
-  console.log(value);
+  if (inputFiled.value === "") {
+    alert("Please Enter the valid data");
+  } else {
+    const value = inputFiled.value;
+    console.log(value);
 
-  let liEle = document.createElement("li");
-  liEle.textContent = value;
-  menuItems.appendChild(liEle);
-  let delEle = document.createElement("Button");
-  delEle.textContent = "Delete";
-  liEle.appendChild(delEle);
-  delEle.classList.add("delete-button");
-  delEle.addEventListener('click', () => {
-    liEle.remove();
-})
+    task.push(value);
+    console.log("Pushing Task", task);
 
-  localStorage.setItem("todoItem", value);
-  inputFiled.value = "";
+    let liEle = document.createElement("li");
+
+    const textSpan = document.createElement("span");
+    textSpan.textContent = value;
+    liEle.appendChild(textSpan);
+
+    // Edit button
+    const editEle = document.createElement("button");
+    editEle.textContent = "Edit";
+    liEle.appendChild(editEle);
+    editEle.classList.add("edit-btn");
+
+    // Delete button
+    let delEle = document.createElement("Button");
+    delEle.textContent = "Delete";
+    liEle.appendChild(delEle);
+    delEle.classList.add("delete-button");
+    menuItems.appendChild(liEle);
+
+    // Edit Feature
+    editEle.addEventListener("click", () => {
+      console.log("I am Editing Button");
+      inputFiled.value = textSpan.textContent;
+      liEle.remove();
+    });
+
+    // Delete Feature
+    delEle.addEventListener("click", () => {
+      liEle.remove();
+    });
+
+    localStorage.setItem("todoItem", value);
+    inputFiled.value = "";
+  }
 });
 
-window.addEventListener('load', () => {
-  const localStorageData = localStorage.getItem("todoItem");
-  console.log(localStorageData);
-})
+// window.addEventListener("load", () => {
+//   const localStorageData = localStorage.getItem("todoItem");
+//   console.log(localStorageData);
+// });
